d73e6aba7640906bf62d8a74efae5583aba9049f
jun <wnstjd117@gmail.com>
2026-07-29 13:56:56 +0900

    chore: 백엔드 초기 프로젝트 설정

---
+++ backend/src/main/java/com/example/findAnswer/FindAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.example.findAnswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAnswerApplication.class, args);
+	}
+
+}
