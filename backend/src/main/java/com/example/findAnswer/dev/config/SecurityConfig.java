8a145e7e7aeed4daa1e6dc985308ed9595e397b8
jun <wnstjd117@gmail.com>
2026-07-30 11:07:40 +0900

    feat: 초기 백엔드 배포 설정

---
+++ backend/src/main/java/com/example/findAnswer/dev/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.findAnswer.dev.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.
+                        anyRequest().permitAll()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .build();
+    }
+}
