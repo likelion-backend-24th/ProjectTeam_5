0c0826f510f190a300ea2094ad414afd755bdb66
jun <wnstjd117@gmail.com>
2026-07-31 09:43:06 +0900

    feat: test swagger-doc 프론트엔드 연결 테스트

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -13,6 +13,14 @@ export default async function  Home() {
     <main>
       <h1>backendStatus</h1>
       <p>{backendStatus}</p>
+
+      <a
+        href="https://api-findanswer.example.com/swagger-ui/index.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Swagger API 문서
+      </a>
     </main>
   )
   
