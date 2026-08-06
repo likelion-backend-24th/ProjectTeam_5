d1cc013131f67afd6cebded8d3ffe0cdc92b30a6
parkjunss <wnstjd117@gmail.com>
2026-08-06 13:11:45 +0900

    Update UserController.java
    Add missing import List

--- backend/src/main/java/com/example/findAnswer/dev/controller/UserController.java
+++ backend/src/main/java/com/example/findAnswer/dev/controller/UserController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
