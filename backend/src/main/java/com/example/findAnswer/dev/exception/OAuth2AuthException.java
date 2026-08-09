bc2bf0c9a5e56cb539e4328a35814a21b264e628
박준성 <wnstjd117@gmail.com>
2026-08-09 12:05:38 +0900

    feat: oauth exception 작성 나중에 보충할것

--- backend/src/main/java/com/example/findAnswer/dev/exception/OAuth2AuthException.java
+++ backend/src/main/java/com/example/findAnswer/dev/exception/OAuth2AuthException.java
@@ -1,9 +1,10 @@
 package com.example.findAnswer.dev.exception;
 
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 
 public class OAuth2AuthException extends OAuth2AuthenticationException {
     public OAuth2AuthException(String message) {
-        super(message);
+        super(new OAuth2Error("oauth2_auth_error"), message);
     }
 }
