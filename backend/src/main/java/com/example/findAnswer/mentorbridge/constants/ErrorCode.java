8d6730bcf4c9d19dc560644900dd4060a45c989d
박준성 <wnstjd117@gmail.com>
2026-08-12 11:48:39 +0900

    feat:ErrorCode FILE_TOO_LARGE 코드 추가

--- backend/src/main/java/com/example/findAnswer/mentorbridge/constants/ErrorCode.java
+++ backend/src/main/java/com/example/findAnswer/mentorbridge/constants/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 질문입니다."),
     ANSWER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 답변입니다."),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    FILE_TOO_LARGE(HttpStatus.BAD_REQUEST, "파일이 너무 큽니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
 
 
