c4e0be0a2d0efbf52356c5b026b85e0ed768c90b
박준성 <wnstjd117@gmail.com>
2026-08-13 13:42:09 +0900

    feat:F-34 결제 수단 등록 삭제 조회 - MockBillingClient Component 빠진거 추가

--- backend/src/main/java/com/example/findAnswer/mentorbridge/client/billing/MockBillingClient.java
+++ backend/src/main/java/com/example/findAnswer/mentorbridge/client/billing/MockBillingClient.java
@@ -3,7 +3,9 @@ package com.example.findAnswer.mentorbridge.client.billing;
 import com.example.findAnswer.mentorbridge.constants.PaymentProvider;
 import com.example.findAnswer.mentorbridge.dto.billing.BillingRegisterResult;
 import com.example.findAnswer.mentorbridge.dto.payment.PaymentMethodRegisterRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MockBillingClient implements BillingClient {
 
     @Override
